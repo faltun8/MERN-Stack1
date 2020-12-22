c4c2347db25aeb49a6378f2b1211f8bd06bad814
furkan <frknaltn08@gmail.com>
2020-12-22 02:08:51 -0500

    change server.js for deployment with react

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ connectDB();
 app.use(express.json());
 app.use("/api", router);
 
+if (process.env.NODE_ENV === "production"){
+  app.use(express.static("client/build"));
+  app.get("*",(req,res) => {
+    res.sendFile(path.resolve(__dirname, "client","build","index.html"))
+  });
+};
+
 const port = process.env.PORT || 5001;
 app.listen(port, () => {
   console.log(`I'm listening on port ${port}`);
